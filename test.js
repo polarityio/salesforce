a72fd84aa45665a56aa36ac38aef88bfb1c6bd6b
Nathan Jenan <nathan.jenan@polarity.io>
2018-03-08 07:26:33 -0700

    Initial commit

---
+++ test.js
@@ -0,0 +1,16 @@
+let assert = require('chai').assert;
+let integration = require('./integration');
+
+describe('salesforce polarity integration', () => {
+    it('should display matching leads', () => {
+
+    });
+
+    it('should display matching contacts', () => {
+
+    });
+
+    it('should display matching opportunities', () => {
+
+    });
+});
