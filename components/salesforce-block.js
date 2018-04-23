51b52da7260cb1b36628df00240aad1812aeb87e
Nathan Jenan <nathan.jenan@polarity.io>
2018-04-23 07:38:26 -0700

    making templates not display empty fields

--- components/salesforce-block.js
+++ components/salesforce-block.js
@@ -19,5 +19,36 @@ polarity.export = PolarityComponent.extend({
     }),
     isOpportunity: Ember.computed('block.data.details', function () {
         return this.get('block.data.details').attributes.type === "Opportunity";
+    }),
+
+    hasTitle: Ember.computed('block.data.details', function () {
+        return !!this.get('block.data.details').Title;
+    }),
+    hasContact: Ember.computed('block.data.details', function () {
+        return this.get('contacts').length > 0;
+    }),
+    hasDepartment: Ember.computed('block.data.details', function () {
+        return !!this.get('block.data.details').Department;
+    }),
+    hasDescription: Ember.computed('block.data.details', function () {
+        return !!this.get('block.data.details').Description;
+    }),
+    hasCompany: Ember.computed('block.data.details', function () {
+        return !!this.get('block.data.details').Company;
+    }),
+    hasStatus: Ember.computed('block.data.details', function () {
+        return !!this.get('block.data.details').Status;
+    }),
+    hasSource: Ember.computed('block.data.details', function () {
+        return !!this.get('block.data.details').Source;
+    }),
+    hasStage: Ember.computed('block.data.details', function () {
+        return !!this.get('block.data.details').Stage;
+    }),
+    hasNextStep: Ember.computed('block.data.details', function () {
+        return !!this.get('block.data.details').NextStep;
+    }),
+    hasExpectedRevenue: Ember.computed('block.data.details', function () {
+        return !!this.get('block.data.details').ExpectedRevenue;
     })
 });
