a80309cd2b8cf290c48edad0d48ef6586fc81dbc
Ed <ed@polarity.io>
2018-06-26 10:32:01 -0400

    Revamp of integration

--- config/config.js
+++ config/config.js
@@ -23,7 +23,7 @@ module.exports = {
      * @optional
      */
     description: "Salesforce provides an interface for case and task management, and routing and escalating important events.",
-    entityTypes: ['string', 'email'],
+    entityTypes: ['email'],
     /**
      * An array of style files (css or less) that will be included for your integration. Any styles specified in
      * the below files can be used in your custom template.
@@ -88,9 +88,8 @@ module.exports = {
         // `/app/polarity-server/integrations/integration-logs`
         // You can also set an absolute path.  If you set an absolute path you must ensure that
         // the directory you specify is writable by the `polarityd:polarityd` user and group.
-
         //directoryPath: '/var/log/polarity-integrations',
-        level: 'trace',  //trace, debug, info, warn, error, fatal
+        level: 'info',  //trace, debug, info, warn, error, fatal
     },
     /**
      * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
@@ -144,6 +143,15 @@ module.exports = {
             type: "password",
             userCanEdit: false,
             adminOnly: true
-        }
+        },
+        {
+            key: "blacklist",
+            name: "Blacklist Domains",
+            description: "Comma delimited list of domains to ignore when sending emails to Salesforce for lookup",
+            default: "",
+            type: "text",
+            userCanEdit: false,
+            adminOnly: false
+        },
     ]
 };
--- integration.js
+++ integration.js
@@ -1,250 +1,271 @@
-let async = require('async');
-let request = require('request');
-let config = require('./config/config');
-let escape = require('./escape');
-let fs = require('fs');
-let requestWithDefaults;
+const fs = require('fs');
+const request = require('request');
+const async = require('async');
+const config = require('./config/config');
+const Salesforce = require('./salesforce');
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+let salesforce;
 let Logger;
-let requestOptions = {
-    json: true,
-    followRedirect: true
-};
 
-function getToken(options, callback) {
-    let requestOptions = {};
-    requestOptions.url = options.host + '/services/oauth2/token';
-    requestOptions.method = 'POST';
-    requestOptions.form = {
-        grant_type: 'password',
-        client_id: options.clientId,
-        client_secret: options.clientSecret,
-        username: options.username,
-        password: options.password
-    };
-
-    //Logger.trace('Token request options', requestOptions);
-
-    requestWithDefaults(requestOptions, (err, resp, body) => {
-        if (err || resp.statusCode != 200) {
-            callback({
-                err: err,
-                body: body,
-                detail: 'Error retrieving Salesforce API token'
-            });
-            return;
-        }
-
-        callback(null, body.access_token);
-    });
-}
-
-// Credit: https://stackoverflow.com/a/46181
-function validEmail(email) {
-    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+/**
+ * Adds stricter email validation
+ * Credit: https://stackoverflow.com/a/46181
+ * @param email
+ * @returns {boolean} True if the email is valid for lookup in salesforce
+ */
+function isValidEmail(email) {
+  return emailRegex.test(String(email).toLowerCase());
 }
 
 function doLookup(entities, options, cb) {
-    let results = [];
+  let blacklistDomains = [];
+  if (blacklistDomains.length > 0) {
+    blacklistDomains = options.blacklist.split(',');
+  }
 
-    getToken(options, (err, accessToken) => {
-        if (err) {
-            Logger.error('Error getting token', {error: err});
-            cb(err);
-            return
-        }
-
-        async.each(entities, (entity, nextEntity) => {
-            if (entity.isEmail && !validEmail(entity.value)) {
-                Logger.trace('Skipping email because it fails validation', entity.value);
-                nextEntity();
-                return;
-            }
-
-            let id = entity.value;
-            requestOptions = {};
-            requestOptions.url = options.host + '/services/data/v20.0/search';
-            requestOptions.qs = {
-                q: 'Find {"' + escape(id) + '"}'
-            };
-            requestOptions.headers = {
-                Authorization: `Bearer ${accessToken}`
-            };
-
-            //Logger.trace('Request options to be sent', {requestOptions: requestOptions});
-
-            requestWithDefaults(requestOptions, (err, resp, body) => {
-                if (err || resp.statusCode != 200) {
-                    Logger.error({err: err, statusCode: resp ? resp.statusCode : null, body: body});
-                    callback({
-                        err: err,
-                        detail: 'Failed to search salesforce',
-                        body: body
-                    });
-                    return;
-                }
-
-                //Logger.trace('Response from server', {body: body});
-
-                if (body.length == 0) {
-                    results.push({entity: entity, data: null});
-                    nextEntity();
-                } else {
-                    async.each(body, (searchResult, nextSearchResult) => {
-                        if (searchResult.attributes.type === 'Contact' ||
-                            searchResult.attributes.type === 'Lead' ||
-                            searchResult.attributes.type === 'Opportunity') {
-
-                            let link = options.host + searchResult.attributes.url;
-                            let requestOptions = {
-                                url: link,
-                                headers: {
-                                    Authorization: `Bearer ${accessToken}`
-                                }
-                            };
-
-                            //Logger.trace({requestOptions: requestOptions});
-
-                            requestWithDefaults(requestOptions, (err, resp, body) => {
-                                if (err || resp.statusCode != 200) {
-                                    nextSearchResult({
-                                        err: err,
-                                        detail: 'Failed to search salesforce',
-                                        body: body
-                                    });
-                                    return;
-                                }
-
-                                //Logger.trace({body: body});
-
-                                body.host = options.host;
-
-                                results.push({
-                                    entity: entity,
-                                    data: {
-                                        details: body
-                                    }
-                                });
-
-                                nextSearchResult();
-                            });
-                        } else {
-                            nextSearchResult();
-                        }
-                    }, err => {
-                        nextEntity(err);
-                    });
-                }
-            });
-        }, err => {
-            Logger.trace({results: results});
-            cb(err, results);
+  entities = entities.reduce((accum, entity) => {
+    if (isValidEmail(entity.value)) {
+      if (blacklistDomains.length > 0) {
+        let result = blacklistDomains.find((domain) => {
+          return entity.value.endsWith(domain.trim());
         });
-    });
+
+        if (!result) {
+          accum.push(entity);
+        }
+      } else {
+        accum.push(entity);
+      }
+    }
+    return accum;
+  }, []);
+
+  Logger.debug({ entities: entities }, 'Entities');
+
+  if (entities.length === 0) {
+    // No valid emails (either invalid or were blacklisted)
+    cb(null, []);
+    return;
+  }
+
+  salesforce.getSummaryByEmail(entities, options, (err, results) => {
+    Logger.debug({ results: results }, 'Results');
+    cb(err, results);
+  });
+}
+
+function _getDetails(id, isContact, options, cb) {
+  async.parallel(
+    {
+      accounts: function(done) {
+        if (isContact === true) {
+          salesforce.getRelatedAccounts(id, options, (err, results) => {
+            done(err, results);
+          });
+        } else {
+          done(null);
+        }
+      },
+      tasks: function(done) {
+        salesforce.getTasks(id, options, (err, results) => {
+          done(err, results);
+        });
+      },
+      opportunities: function(done) {
+        if (isContact === true) {
+          salesforce.getOpportunities(id, options, (err, results) => {
+            done(err, results);
+          });
+        } else {
+          done(null);
+        }
+      },
+      campaigns: function(done) {
+        if (isContact === true) {
+          salesforce.getCampaignsByContact(id, options, (err, results) => {
+            done(err, results);
+          });
+        } else {
+          salesforce.getCampaignsByLead(id, options, (err, results) => {
+            done(err, results);
+          });
+        }
+      }
+    },
+    cb
+  );
+}
+
+function onDetails(lookupObject, options, cb) {
+  async.waterfall(
+    [
+      function getContactDetails(done) {
+        async.each(
+          lookupObject.data.details.contacts,
+          (contact, next) => {
+            _getDetails(contact.contactId, contact.isContact, options, (err, results) => {
+              contact.accounts = results.accounts;
+              contact.tasks = results.tasks;
+              contact.opportunities = results.opportunities;
+              contact.campaigns = results.campaigns;
+              next(err);
+            });
+          },
+          done
+        );
+      },
+      function getLeadDetails(done) {
+        async.each(
+          lookupObject.data.details.leads,
+          (lead, next) => {
+            _getDetails(lead.leadId, lead.isLead, options, (err, results) => {
+              lead.tasks = results.tasks;
+              lead.campaigns = results.campaigns;
+              next(err);
+            });
+          },
+          done
+        );
+      }
+    ],
+    function(err) {
+      Logger.debug({ lookupObject: lookupObject }, 'onDetails');
+      cb(err, lookupObject.data);
+    }
+  );
 }
 
 function startup(logger) {
-    Logger = logger;
+  Logger = logger;
+  let requestOptions = {};
 
-    if (typeof config.request.cert === 'string' && config.request.cert.length > 0) {
-        requestOptions.cert = fs.readFileSync(config.request.cert);
-    }
+  if (typeof config.request.cert === 'string' && config.request.cert.length > 0) {
+    requestOptions.cert = fs.readFileSync(config.request.cert);
+  }
 
-    if (typeof config.request.key === 'string' && config.request.key.length > 0) {
-        requestOptions.key = fs.readFileSync(config.request.key);
-    }
+  if (typeof config.request.key === 'string' && config.request.key.length > 0) {
+    requestOptions.key = fs.readFileSync(config.request.key);
+  }
 
-    if (typeof config.request.passphrase === 'string' && config.request.passphrase.length > 0) {
-        requestOptions.passphrase = config.request.passphrase;
-    }
+  if (typeof config.request.passphrase === 'string' && config.request.passphrase.length > 0) {
+    requestOptions.passphrase = config.request.passphrase;
+  }
 
-    if (typeof config.request.ca === 'string' && config.request.ca.length > 0) {
-        requestOptions.ca = fs.readFileSync(config.request.ca);
-    }
+  if (typeof config.request.ca === 'string' && config.request.ca.length > 0) {
+    requestOptions.ca = fs.readFileSync(config.request.ca);
+  }
 
-    if (typeof config.request.proxy === 'string' && config.request.proxy.length > 0) {
-        requestOptions.proxy = config.request.proxy;
-    }
+  if (typeof config.request.proxy === 'string' && config.request.proxy.length > 0) {
+    requestOptions.proxy = config.request.proxy;
+  }
 
-    if (typeof config.request.rejectUnauthorized === 'boolean') {
-        requestOptions.rejectUnauthorized = config.request.rejectUnauthorized;
-    }
+  if (typeof config.request.rejectUnauthorized === 'boolean') {
+    requestOptions.rejectUnauthorized = config.request.rejectUnauthorized;
+  }
+  let requestWithDefaults = request.defaults(requestOptions);
 
-    requestWithDefaults = request.defaults(requestOptions);
+  salesforce = new Salesforce(requestWithDefaults, logger);
 }
 
 function validateOption(errors, options, optionName, errMessage) {
-    if (!options[optionName] || typeof options[optionName].value !== 'string' ||
-        (typeof options[optionName].value === 'string' && options[optionName].value.length === 0)) {
-        errors.push({
-            key: optionName,
-            message: errMessage
-        });
-    }
+  if (
+    !options[optionName] ||
+    typeof options[optionName].value !== 'string' ||
+    (typeof options[optionName].value === 'string' && options[optionName].value.length === 0)
+  ) {
+    errors.push({
+      key: optionName,
+      message: errMessage
+    });
+  }
 }
 
 function validateCanLogin(options, callback) {
+  let opts = {};
+  for (let k in options) {
+    opts[k] = options[k].value;
+  }
 
-    let opts = {};
-    for (let k in options) {
-        opts[k] = options[k].value;
+  salesforce.validateCredentials(opts, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null);
     }
-
-    getToken(opts, (err) => {
-        if (err) {
-            callback('could not authorize with Salesforce: ' + JSON.stringify(err));
-        } else {
-            Logger.debug('Succesfully validated authentication credentials');
-            callback(null);
-        }
-    });
+  });
 }
 
 function validateOptions(options, callback) {
-    Logger.trace('Options to validate', options);
+  Logger.trace('Options to validate', options);
 
-    let errors = [];
+  let errors = [];
 
-    validateOption(errors, options, 'host', 'You must provide a valid host for the Salesforce instance.');
-    validateOption(errors, options, 'clientId', 'You must provide a valid client id for the Salesforce instance.');
-    validateOption(errors, options, 'clientSecret', 'You must provide a valid client secret for the Salesforce instance.');
-    validateOption(errors, options, 'username', 'You must provide a valid username for the Salesforce instance.');
-    validateOption(errors, options, 'password', 'You must provide a valid password + security token for the Salesforce instance.');
+  validateOption(
+    errors,
+    options,
+    'host',
+    'You must provide a valid host for the Salesforce instance.'
+  );
+  validateOption(
+    errors,
+    options,
+    'clientId',
+    'You must provide a valid client id for the Salesforce instance.'
+  );
+  validateOption(
+    errors,
+    options,
+    'clientSecret',
+    'You must provide a valid client secret for the Salesforce instance.'
+  );
+  validateOption(
+    errors,
+    options,
+    'username',
+    'You must provide a valid username for the Salesforce instance.'
+  );
+  validateOption(
+    errors,
+    options,
+    'password',
+    'You must provide a valid password + security token for the Salesforce instance.'
+  );
 
-    if (errors.length === 0) {
-        validateCanLogin(options, (err) => {
-            if (err) {
-                errors.push({
-                    key: 'clientId',
-                    message: err
-                });
-
-                errors.push({
-                    key: 'clientSecret',
-                    message: err
-                });
-
-                errors.push({
-                    key: 'username',
-                    message: err
-                });
-
-                errors.push({
-                    key: 'password',
-                    message: err
-                });
-            }
-
-            callback(null, errors);
+  if (errors.length === 0) {
+    validateCanLogin(options, (err) => {
+      if (err) {
+        errors.push({
+          key: 'clientId',
+          message: err
         });
-    } else {
-        callback(null, errors);
-    }
+
+        errors.push({
+          key: 'clientSecret',
+          message: err
+        });
+
+        errors.push({
+          key: 'username',
+          message: err
+        });
+
+        errors.push({
+          key: 'password',
+          message: err
+        });
+      }
+
+      callback(null, errors);
+    });
+  } else {
+    callback(null, errors);
+  }
 }
 
 module.exports = {
-    doLookup: doLookup,
-    startup: startup,
-    validateOptions: validateOptions
+  doLookup: doLookup,
+  startup: startup,
+  onDetails: onDetails,
+  validateOptions: validateOptions
 };
